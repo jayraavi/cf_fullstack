39751ebb4e9aada4e64afa885fa0938a9fe2fd59
Vijay Raavi <vraavi@calpoly.edu>
2020-04-19 15:29:11 -0700

    Added fetchUrls method to get variants array

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+const variants_url = new URL(
+  "https://cfw-takehome.developers.workers.dev/api/variants"
+);
+
+async function fetchUrls() {
+  let response = await fetch(variants_url).then((resp) => resp.json());
+  console.log(response);
+  return response.variants;
+}
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  let variants = await fetchUrls();
+  console.log(variants);
+  return new Response("Hello worker!", {
+    headers: { "content-type": "text/plain" },
+  });
 }
