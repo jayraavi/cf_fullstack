e461439e7a46fe66b8fb1c0afd5c96c198acaec6
Vijay Raavi <vraavi@calpoly.edu>
2020-04-19 21:04:25 -0700

    Fetch request to random variant, display response HTML

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const variants_url = new URL(
+const VARIANTS_URL = new URL(
   "https://cfw-takehome.developers.workers.dev/api/variants"
 );
+const rewriter = new HTMLRewriter();
 
 async function fetchUrls() {
-  let response = await fetch(variants_url).then((resp) => resp.json());
+  let response = await fetch(VARIANTS_URL).then((resp) => resp.json());
   console.log(response);
   return response.variants;
+}
+
+function pickRandomVariant(variants) {
+  const RANDOM_INDEX = Math.floor(Math.random() * 2);
+  return new URL(variants[RANDOM_INDEX]);
+}
+
+async function fetchVariantContent(variant) {
+  let response = await fetch(variant);
+  return rewriter.transform(response);
 }
 
 addEventListener("fetch", (event) => {
@@ -17,8 +28,8 @@ addEventListener("fetch", (event) => {
  */
 async function handleRequest(request) {
   let variants = await fetchUrls();
-  console.log(variants);
-  return new Response("Hello worker!", {
-    headers: { "content-type": "text/plain" },
-  });
+  variant = pickRandomVariant(variants);
+  console.log(variant);
+  let body = await fetchVariantContent(variant);
+  return body;
 }
